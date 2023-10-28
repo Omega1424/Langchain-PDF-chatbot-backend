33306193eba8876ba3248dd54ef35d3f26de3e27
Omega1424 <vinuisaac@hotmail.com>
2023-10-29 00:00:08 +0800

    fixed database loading from previous session

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from multi_doc_chatbot import ask_chatbot, document_loader, create_embeddings, tools
+from multi_doc_chatbot import ask_chatbot, document_loader, tools
 
 app = FastAPI()
 
@@ -10,9 +10,6 @@ def ask_endpoint(query: str= "Who is Juan Garcia?"):
 
 @app.get('/embed')
 def get_context():
-    if tools.qa_chain is None:
-        docs = document_loader()
-        create_embeddings(docs, persist_directory='/root/langchain-chatbot/data')
-        return {"status": "success", "message": "Embeddings created and QA chain initialized."}
-    else:
-        return {"status": "info", "message": "Embeddings and QA chain are already initialized."}
+    docs = document_loader()
+    tools.create_embeddings(docs, persist_directory='/root/langchain-chatbot/data')
+    return {"status": "success", "message": "Embeddings created. "}
--- multi_doc_chatbot.py
+++ multi_doc_chatbot.py
@@ -1,3 +1,4 @@
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 import os
@@ -15,36 +16,36 @@ if openai_key is None:
 
 class ChatbotTools:
     _instance = None  # Singleton instance
-    
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(ChatbotTools, cls).__new__(cls)
-            cls._instance.vectordb = None
-            cls._instance.qa_chain = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ChatbotTools, cls).__new__(cls, *args, **kwargs)
         return cls._instance
     
     def __init__(self):
-        self.vectordb = None
+        self.persist_directory = '/root/langchain-chatbot/data'
+        self.vectordb = Chroma(persist_directory=self.persist_directory, embedding_function = OpenAIEmbeddings())
         self.qa_chain = None
-    
+
+
     def create_vectordb(self, documents, persist_directory):
         self.vectordb = Chroma.from_documents(
             documents,
             embedding=OpenAIEmbeddings(),
-            persist_directory=persist_directory
+            persist_directory=self.persist_directory
         )
         self.vectordb.persist()
-        
-    def create_qa_chain(self):
-        if self.vectordb is None:
-            raise ValueError("vectordb needs to be created first!")
-        
+
+    def create_qa_chain(self):            
         self.qa_chain = ConversationalRetrievalChain.from_llm(
             ChatOpenAI(model_name="gpt-3.5-turbo"),
             self.vectordb.as_retriever(search_kwargs={'k': 3}),
             return_source_documents=True
         )
-
+    def create_embeddings(self,documents, persist_directory):
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+        chunked_documents = text_splitter.split_documents(documents)
+        self.create_vectordb(chunked_documents, persist_directory)
 
 tools = ChatbotTools()  
 
@@ -66,13 +67,10 @@ def document_loader():
     return documents
 
 # we split the data into chunks of 1,000 characters, with an overlap of 200 characters between the chunks, which helps to give better results and contain the context of the information between chunks
-persist_directory = '/root/langchain-chatbot/data'
 
-def create_embeddings(documents, persist_directory):
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
-    chunked_documents = text_splitter.split_documents(documents)
-    tools.create_vectordb(documents, persist_directory)  # Using chunked_documents
-    tools.create_qa_chain()
+
+  # Using chunked_documents
+
 
 chat_history = []
 
@@ -82,14 +80,12 @@ def ask_chatbot(query):
     prompt = query + "Answer the question based on only the context given. If you don't know the answer say I don't know"
     
     if tools.qa_chain is None:
-        raise ValueError("The qa_chain hasn't been initialized!")
+        tools.create_qa_chain()
     
     result = tools.qa_chain({'question': prompt, 'chat_history': chat_history})
     answer = result.get('answer', "Sorry, I don't know")
-    chat_history.append({
-        'query': query,
-        'answer': answer
-    })
+    chat_history.append((query,answer))
+
     if len(chat_history) > 4:
         chat_history.pop(0)
     return answer
